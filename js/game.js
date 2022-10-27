cfa2a001075e742090278819d555fb0a91d6461e
FabianPavel <pavel.fabian05@gmail.com>
2022-10-27 18:42:19 +0100

    ascend table with info, text for being poor

--- js/game.js
+++ js/game.js
@@ -4,6 +4,8 @@ const c = document.getElementById('coins');
 const autoUp = document.getElementById('autoEarn');
 const timeA = document.getElementById('autoTime');
 const ascend = document.getElementById("ascend");
+const ascend2 = document.getElementById("ascend2");
+const close =  document.getElementById("close");
 
 const hack = document.getElementById("hack");
 let ezMoney = 0;
@@ -13,6 +15,8 @@ let time = 2000;
 let exponent = 1.4;
 let a = 0;
 let ascendBonus = 0;
+let autoAs = 0;
+let timeout = 0;
 
 let coolDown = 400;
 let timer = Date.now();
@@ -36,19 +40,19 @@ function auto(){
 
 setInterval( function (){
     if(coins < 1000){
-        c.innerHTML = `your coins:` + (coins).toFixed(3);
+        c.innerHTML = `your coins:` + (coins).toFixed(2);
     }
     else if(coins >= 1000 && coins < 1000000){
-        c.innerHTML = `your coins: ${(coins / 1000).toFixed(3)} K`;
+        c.innerHTML = `your coins: ${(coins / 1000).toFixed(2)} K`;
     }
     else if(coins >= 1000000 && coins < 1000000000){
-        c.innerHTML = `your coins: ${(coins / 1000).toFixed(3)} M`;
+        c.innerHTML = `your coins: ${(coins / 1000).toFixed(2)} M`;
     }
     else if(coins >= 1000000000 && coins < 1000000000000){
-        c.innerHTML = `your coins: ${(coins / 1000).toFixed(3)}B`;
+        c.innerHTML = `your coins: ${(coins / 1000).toFixed(2)}B`;
     }
     else if(coins >= 1000000000000 && coins < 1000000000000000) {
-        c.innerHTML = `your coins: ${(coins / 1000).toFixed(3)}T`;
+        c.innerHTML = `your coins: ${(coins / 1000).toFixed(2)}T`;
     }
 }, 10);
 
@@ -68,6 +72,13 @@ clickUp.addEventListener('click', function(){
        clickOrder += 1;
        clickUp.innerHTML = `upgrade ${clickOrder} price: ${clickPrice}`;
    }
+   else{
+       document.getElementById("hidden").style.display = "inline";
+
+       timeout = setTimeout(() =>{
+           document.getElementById("hidden").style.display = "none";
+       },1000);
+   }
 });
 
 autoUp.addEventListener('click', function(){
@@ -76,8 +87,8 @@ autoUp.addEventListener('click', function(){
        autoPrice = Math.ceil(autoPrice * (exponent + autoOrder / 2));
        autoOrder += 1;
        autoUp.innerHTML = `auto ${autoOrder} price: ${autoPrice}`;
-       setInterval(auto, time);
        a = 1;
+       autoAs = setInterval(auto, time);
    }
 });
 
@@ -89,19 +100,46 @@ timeA.addEventListener('click',  () =>{
         console.log(time);
         timeOrder += 1;
         timeA.innerHTML = `auto ${timeOrder} price: ${timePrice}`;
-        if(a === 1){
-           setInterval(auto, time);
-        }
+       if(a === 1){
+           autoAs =setInterval(auto, time);
+       }
    }
 });
 
 ascend.addEventListener('click', () =>{
+    //show div with info about ascend + close/continue
+    document.getElementById("hid").style.display = "inline";
+    //run ascend
+    ascend2.addEventListener('click', () =>{
+    //calculate ascend bonus, set coins back to 0
     ascendBonus += (coins / 100000);
     coins = 0;
+    //make earns bigger after ascend using ascend bonus
     asEarns += ascendBonus;
     earns = asEarns;
     asAutoEarns += ascendBonus;
     autoEarns = asAutoEarns;
+    //set variables back to start value
+    autoOrder = 1;
+    clickOrder = 1;
+    timeOrder = 1;
+    time = 2000;
+    timePrice = 40;
+    autoPrice = 20;
+    clickPrice = 10;
+    //clear auto earning
+    a = 0;
+    clearInterval(autoAs);
+    //again broadcast level and price of upgrades
+    clickUp.innerHTML = `upgrade ${clickOrder} price: ${clickPrice}`;
+    autoUp.innerHTML = `auto ${autoOrder} price: ${autoPrice}`;
+    timeA.innerHTML = `auto ${timeOrder} price: ${timePrice}`;
+    document.getElementById("hid").style.display = "none";
+    });
+    //close div with info about ascend + close/continue
+    close.addEventListener('click', () => {
+        document.getElementById("hid").style.display = "none";
+    });
 });
 
 hack.addEventListener('click', () => {
